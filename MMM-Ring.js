c1d84d68e2e3f1986db8127ca996321c3c723a07
Rivelia <kevisor@gmail.com>
2020-12-31 15:56:06 +0100

    Audio cues on motion/doorbell ring, screen wakeup from PIR sensor module.

--- MMM-Ring.js
+++ MMM-Ring.js
@@ -10,140 +10,150 @@
  * https://github.com/dgreif/ring
  */
 
-Module.register("MMM-Ring", {
-  DisplayTypes: {
-    NONE: 1,
-    ERROR: 2,
-    VIDEO: 3
-  },
+Module.register('MMM-Ring', {
+	DisplayTypes: {
+		NONE: 1,
+		ERROR: 2,
+		VIDEO: 3,
+	},
 
-  defaults: {
-    ringEmail: undefined,
-    ringPwd: undefined,
-    ringStreamMotion: false,
-    ring2faRefreshToken: undefined,
-    ringMinutesToStreamVideo: 1.5,
-    ringVideoWidth: "600"
-  },
+	defaults: {
+		ringEmail: undefined,
+		ringPwd: undefined,
+		ringStreamMotion: false,
+		ring2faRefreshToken: undefined,
+		ringMinutesToStreamVideo: 1.5,
+		ringVideoWidth: '600',
+		ringWakesUpScreen: true,
+	},
 
-  start: function () {
-    this.errorMessage = "";
-    this.displayType = this.DisplayTypes.NONE;
-    this.hls = "";
+	start: function () {
+		this.errorMessage = '';
+		this.displayType = this.DisplayTypes.NONE;
+		this.hls = '';
 
-    if (
-      this.config.ringEmail !== undefined ||
-      this.config.ringPwd !== undefined
-    ) {
-      this.displayType = this.DisplayTypes.ERROR;
-      this.errorMessage =
-        "ringEmail and ringPwd are no longer valid configuration properties. Ring now requires a 2 factor authentication (2fa) refresh token. Must use ring2faRefreshToken property in config.";
-      return;
-    }
+		if (
+			this.config.ringEmail !== undefined ||
+			this.config.ringPwd !== undefined
+		) {
+			this.displayType = this.DisplayTypes.ERROR;
+			this.errorMessage =
+				'ringEmail and ringPwd are no longer valid configuration properties. Ring now requires a 2 factor authentication (2fa) refresh token. Must use ring2faRefreshToken property in config.';
+			return;
+		}
 
-    if (this.config.ring2faRefreshToken === undefined) {
-      this.displayType = this.DisplayTypes.ERROR;
-      this.errorMessage =
-        "Must provide ring2faRefreshToken within the MMM-Ring configuration within the Magic Mirror config file.";
-      return;
-    }
+		if (this.config.ring2faRefreshToken === undefined) {
+			this.displayType = this.DisplayTypes.ERROR;
+			this.errorMessage =
+				'Must provide ring2faRefreshToken within the MMM-Ring configuration within the Magic Mirror config file.';
+			return;
+		}
 
-    if (this.config.ringMinutesToStreamVideo > 5) {
-      this.displayType = this.DisplayTypes.ERROR;
-      this.errorMessage =
-        "ringMinutesToStreamVideo configuration property can not be larger than 5";
-      return;
-    }
+		if (this.config.ringMinutesToStreamVideo > 5) {
+			this.displayType = this.DisplayTypes.ERROR;
+			this.errorMessage =
+				'ringMinutesToStreamVideo configuration property can not be larger than 5';
+			return;
+		}
 
-    this.sendSocketNotification("BEGIN_RING_MONITORING", this.config);
-  },
+		this.sendSocketNotification('BEGIN_RING_MONITORING', this.config);
+	},
 
-  getScripts: function () {
-    return ["https://cdn.jsdelivr.net/npm/hls.js"];
-  },
+	getScripts: function () {
+		return ['https://cdn.jsdelivr.net/npm/hls.js'];
+	},
 
-  getStyles: function () {
-    return ["MMM-Ring.css"];
-  },
+	getStyles: function () {
+		return ['MMM-Ring.css'];
+	},
 
-  requiresVersion: "2.1.0", // Required version of MagicMirror
+	requiresVersion: '2.1.0', // Required version of MagicMirror
 
-  getDom: function () {
-    if (this.hls) {
-      this.hls.destroy();
-      this.hls = null;
-    }
+	getDom: function () {
+		if (this.hls) {
+			this.hls.destroy();
+			this.hls = null;
+		}
 
-    var wrapper = document.createElement("div");
+		var wrapper = document.createElement('div');
 
-    switch (this.displayType) {
-      case this.DisplayTypes.NONE:
-        return wrapper;
-      case this.DisplayTypes.ERROR:
-        wrapper.innerHTML = this.errorMessage;
-        return wrapper;
-      case this.DisplayTypes.VIDEO:
-        var streamPath = window.location.href + "MMM-Ring/stream.m3u8";
-        var video = document.createElement("video");
-        video.className = "video";
-        video.width = this.config.ringVideoWidth;
-        video.muted = true;
-        wrapper.appendChild(video);
+		switch (this.displayType) {
+			case this.DisplayTypes.NONE:
+				return wrapper;
+			case this.DisplayTypes.ERROR:
+				wrapper.innerHTML = this.errorMessage;
+				return wrapper;
+			case this.DisplayTypes.VIDEO:
+				var streamPath = window.location.href + 'MMM-Ring/stream.m3u8';
+				var video = document.createElement('video');
+				video.className = 'video';
+				video.width = this.config.ringVideoWidth;
+				video.muted = true;
+				wrapper.appendChild(video);
 
-        if (Hls.isSupported()) {
-          const config = { liveDurationInfinity: true };
+				if (Hls.isSupported()) {
+					const config = { liveDurationInfinity: true };
 
-          var hls = new Hls(config);
-          this.hls = hls;
+					var hls = new Hls(config);
+					this.hls = hls;
 
-          hls.on(Hls.Events.ERROR, function (event, data) {
-            var errorType = data.type;
-            var errorDetails = data.details;
-            var errorFatal = data.fatal;
-            Log.error(
-              `***************** MMM-Ring ERROR! Type: ${errorType}, Details: ${errorDetails}, Fatal: ${errorFatal}`
-            );
-          });
+					hls.on(Hls.Events.ERROR, function (event, data) {
+						var errorType = data.type;
+						var errorDetails = data.details;
+						var errorFatal = data.fatal;
+						Log.error(
+							`***************** MMM-Ring ERROR! Type: ${errorType}, Details: ${errorDetails}, Fatal: ${errorFatal}`
+						);
+					});
 
-          hls.attachMedia(video);
+					hls.attachMedia(video);
 
-          hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-            hls.loadSource(streamPath);
-            hls.on(Hls.Events.MANIFEST_PARSED, function () {
-              video.play();
-            });
-          });
-        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-          video.src = streamPath;
-          video.addEventListener("loadedmetadata", function () {
-            video.play();
-          });
-        }
+					hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+						hls.loadSource(streamPath);
+						hls.on(Hls.Events.MANIFEST_PARSED, function () {
+							video.play();
+						});
+					});
+				} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+					video.src = streamPath;
+					video.addEventListener('loadedmetadata', function () {
+						video.play();
+					});
+				}
 
-        return wrapper;
-    }
-  },
+				return wrapper;
+		}
+	},
 
-  socketNotificationReceived: function (notification, payload) {
-    switch (notification) {
-      case "DISPLAY_ERROR":
-        this.displayType = this.DisplayTypes.ERROR;
-        this.errorMessage = payload;
-        this.updateDom();
-        break;
-      case "VIDEO_STREAM_ENDED":
-        this.displayType = this.DisplayTypes.NONE;
-        this.updateDom();
-        break;
-      case "VIDEO_STREAM_AVAILABLE":
-        // if we are already on video stream then bail
-        if (this.displayType === this.DisplayTypes.VIDEO) {
-          break;
-        }
+	socketNotificationReceived: function (notification, payload) {
+		switch (notification) {
+			case 'DISPLAY_ERROR':
+				this.displayType = this.DisplayTypes.ERROR;
+				this.errorMessage = payload;
+				this.updateDom();
+				break;
+			case 'VIDEO_STREAM_ENDED':
+				this.displayType = this.DisplayTypes.NONE;
+				this.updateDom();
+				break;
+			case 'VIDEO_STREAM_AVAILABLE':
+				// if we are already on video stream then bail
+				if (this.displayType === this.DisplayTypes.VIDEO) {
+					break;
+				}
 
-        this.displayType = this.DisplayTypes.VIDEO;
-        this.updateDom();
-        break;
-    }
-  }
+				this.displayType = this.DisplayTypes.VIDEO;
+				this.updateDom();
+				break;
+			case 'DOORBELL_RINGING':
+				new Audio('MMM-Ring/doorbell_ringing.mp3').play();
+				break;
+			case 'MOTION_DETECTED':
+				new Audio('MMM-Ring/motion_detected.wav').play();
+				break;
+			case 'SCREEN_WAKEUP':
+				this.sendNotification(notification, payload);
+				break;
+		}
+	},
 });
--- node_helper.js
+++ node_helper.js
@@ -10,248 +10,279 @@
  * https://github.com/dgreif/ring
  */
 
-const NodeHelper = require("node_helper");
-const pathApi = require("path");
-const util = require("util");
-const fs = require("fs");
-const mainRingApi = require("ring-client-api");
-const operators = require("rxjs/operators");
-const fileWatcher = require("chokidar");
+const NodeHelper = require('node_helper');
+const pathApi = require('path');
+const util = require('util');
+const fs = require('fs');
+const mainRingApi = require('ring-client-api');
+const operators = require('rxjs/operators');
+const fileWatcher = require('chokidar');
 
 module.exports = NodeHelper.create({
-  requiresVersion: "2.11.0",
+	requiresVersion: '2.11.0',
 
-  start: function () {
-    this.toLog("Starting module: MMM-Ring");
-    this.videoOutputDirectory = pathApi.join(this.path, "public");
-    this.envFile = pathApi.join(this.path, ".env");
-    this.ringApi = null;
-    this.config = null;
-    this.watcher = null;
-    this.audioPlaylistFile = "stream.m3u8";
-    this.sipSession = null;
-    this.sessionRunning = false;
-  },
+	start: function () {
+		this.toLog('Starting module: MMM-Ring');
+		this.videoOutputDirectory = pathApi.join(this.path, 'public');
+		this.envFile = pathApi.join(this.path, '.env');
+		this.ringApi = null;
+		this.config = null;
+		this.watcher = null;
+		this.audioPlaylistFile = 'stream.m3u8';
+		this.sipSession = null;
+		this.sessionRunning = false;
 
-  stop: function () {
-    this.toLog("Stopping module helper: MMM-Ring");
-    this.stopWatchingFile();
+		this.cleanUpVideoStreamDirectory();
+	},
 
-    if (this.sipSession) {
-      this.sipSession.stop();
-      this.sipSession = null;
-    }
+	stop: function () {
+		this.toLog('Stopping module helper: MMM-Ring');
+		this.stopWatchingFile();
 
-    this.cleanUpVideoStreamDirectory();
-  },
+		if (this.sipSession) {
+			this.sipSession.stop();
+			this.sipSession = null;
+		}
 
-  socketNotificationReceived: async function (notification, payload) {
-    if (notification === "BEGIN_RING_MONITORING") {
-      this.config = payload;
+		this.cleanUpVideoStreamDirectory();
+	},
 
-      if (!(await util.promisify(fs.exists)(this.envFile))) {
-        await util.promisify(fs.writeFile)(
-          this.envFile,
-          `RING_2FA_REFRESH_TOKEN=${this.config.ring2faRefreshToken}`
-        );
-      }
+	socketNotificationReceived: async function (notification, payload) {
+		if (notification === 'BEGIN_RING_MONITORING') {
+			this.config = payload;
 
-      require("dotenv").config({ path: this.envFile });
+			if (!(await util.promisify(fs.exists)(this.envFile))) {
+				await util.promisify(fs.writeFile)(
+					this.envFile,
+					`RING_2FA_REFRESH_TOKEN=${this.config.ring2faRefreshToken}`
+				);
+			}
 
-      this.monitorRingActivity();
-    }
-  },
+			require('dotenv').config({ path: this.envFile });
 
-  cleanUpVideoStreamDirectory: async function () {
-    if (!(await util.promisify(fs.exists)(this.videoOutputDirectory))) {
-      await util.promisify(fs.mkdir)(this.videoOutputDirectory);
-    }
+			this.monitorRingActivity();
+		}
+	},
 
-    const files = await util.promisify(fs.readdir)(this.videoOutputDirectory);
-    const unlinkPromises = files.map((filename) =>
-      util.promisify(fs.unlink)(`${this.videoOutputDirectory}/${filename}`)
-    );
-    return await Promise.all(unlinkPromises);
-  },
+	cleanUpVideoStreamDirectory: async function () {
+		if (!(await util.promisify(fs.exists)(this.videoOutputDirectory))) {
+			await util.promisify(fs.mkdir)(this.videoOutputDirectory);
+		}
 
-  monitorRingActivity: async function () {
-    this.ringApi = new mainRingApi.RingApi({
-      refreshToken: process.env.RING_2FA_REFRESH_TOKEN,
-      debug: true,
-      cameraDingsPollingSeconds: 2
-    });
+		let files = await util.promisify(fs.readdir)(
+			this.videoOutputDirectory,
+			{ withFileTypes: true }
+		);
+		files = files.filter(
+			(obj) => !obj.name.endsWith('.mp3') && !obj.name.endsWith('.wav')
+		);
 
-    this.ringApi.onRefreshTokenUpdated.subscribe(
-      async ({ newRefreshToken, oldRefreshToken }) => {
-        this.toLog("Refresh Token Updated");
+		const unlinkPromises = files.map((obj) =>
+			util.promisify(fs.unlink)(
+				`${this.videoOutputDirectory}/${obj.name}`
+			)
+		);
+		return await Promise.all(unlinkPromises);
+	},
 
-        if (!oldRefreshToken) {
-          return;
-        }
+	monitorRingActivity: async function () {
+		this.ringApi = new mainRingApi.RingApi({
+			refreshToken: process.env.RING_2FA_REFRESH_TOKEN,
+			debug: true,
+			cameraDingsPollingSeconds: 2,
+		});
 
-        const currentConfig = await util.promisify(fs.readFile)(this.envFile);
-        const updateConfig = currentConfig
-          .toString()
-          .replace(oldRefreshToken, newRefreshToken);
-        await util.promisify(fs.writeFile)(this.envFile, updateConfig);
-      }
-    );
+		this.ringApi.onRefreshTokenUpdated.subscribe(
+			async ({ newRefreshToken, oldRefreshToken }) => {
+				this.toLog('Refresh Token Updated');
 
-    const locations = await this.ringApi.getLocations();
-    const allCameras = await this.ringApi.getCameras();
+				if (!oldRefreshToken) {
+					return;
+				}
 
-    this.toLog(
-      `Found ${locations.length} location(s) with ${allCameras.length} camera(s).`
-    );
+				const currentConfig = await util.promisify(fs.readFile)(
+					this.envFile
+				);
+				const updateConfig = currentConfig
+					.toString()
+					.replace(oldRefreshToken, newRefreshToken);
+				await util.promisify(fs.writeFile)(this.envFile, updateConfig);
+			}
+		);
 
-    for (const location of locations) {
-      location.onConnected.pipe(operators.skip(1)).subscribe((connected) => {
-        const status = connected ? "Connected to" : "Disconnected from";
-        this.toLog(
-          `**** ${status} location ${location.locationDetails.name} - ${location.locationId}`
-        );
-      });
-    }
+		const locations = await this.ringApi.getLocations();
+		const allCameras = await this.ringApi.getCameras();
 
-    for (const location of locations) {
-      const cameras = location.cameras,
-        devices = await location.getDevices();
+		this.toLog(
+			`Found ${locations.length} location(s) with ${allCameras.length} camera(s).`
+		);
 
-      this.toLog(
-        `Location ${location.locationDetails.name} has the following ${cameras.length} camera(s):`
-      );
+		for (const location of locations) {
+			location.onConnected
+				.pipe(operators.skip(1))
+				.subscribe((connected) => {
+					const status = connected
+						? 'Connected to'
+						: 'Disconnected from';
+					this.toLog(
+						`**** ${status} location ${location.locationDetails.name} - ${location.locationId}`
+					);
+				});
+		}
 
-      for (const camera of cameras) {
-        this.toLog(`- ${camera.id}: ${camera.name} (${camera.deviceType})`);
-      }
+		for (const location of locations) {
+			const cameras = location.cameras,
+				devices = await location.getDevices();
 
-      this.toLog(
-        `Location ${location.locationDetails.name} has the following ${devices.length} device(s):`
-      );
+			this.toLog(
+				`Location ${location.locationDetails.name} has the following ${cameras.length} camera(s):`
+			);
 
-      for (const device of devices) {
-        this.toLog(`- ${device.zid}: ${device.name} (${device.deviceType})`);
-      }
-    }
+			for (const camera of cameras) {
+				this.toLog(
+					`- ${camera.id}: ${camera.name} (${camera.deviceType})`
+				);
+			}
 
-    if (allCameras === undefined || allCameras.length == 0) {
-      this.toLog(
-        `no cameras were found! Ensure you have a camera on your account or that you provided accurate login information in the config. You'll need to restart MagicMirror.`
-      );
-      this.sendSocketNotification(
-        "DISPLAY_ERROR",
-        "No cameras were found! Check console for more info."
-      );
-      return;
-    }
+			this.toLog(
+				`Location ${location.locationDetails.name} has the following ${devices.length} device(s):`
+			);
 
-    // Start listening for doorbell presses on each camera
-    allCameras.forEach((camera) => {
-      camera.onDoorbellPressed.subscribe(async () => {
-        if (!this.sipSession) {
-          await this.startSession(camera, "ring");
-        }
-      });
-      this.toLog(`Actively listening for doorbell presses`);
-      //Check config value if node app should stream motion
-      if(this.config.ringStreamMotion){
-        camera.onMotionDetected.subscribe(async (newMotion) => {
-          //NewMotion is a true false value indicating whether the motion is new based on the dings made in the last 65 seconds
-          // This prevents the stream from being triggered on startup because it would not be an active motion event.
-          if (!this.sipSession && newMotion) {
-            await this.startSession(camera, "motion");
-          }
-        });
-        this.toLog(`Actively listening for Motion events`);
-      }
-      
-    });
+			for (const device of devices) {
+				this.toLog(
+					`- ${device.zid}: ${device.name} (${device.deviceType})`
+				);
+			}
+		}
 
-    
-  },
+		if (allCameras === undefined || allCameras.length == 0) {
+			this.toLog(
+				`no cameras were found! Ensure you have a camera on your account or that you provided accurate login information in the config. You'll need to restart MagicMirror.`
+			);
+			this.sendSocketNotification(
+				'DISPLAY_ERROR',
+				'No cameras were found! Check console for more info.'
+			);
+			return;
+		}
 
-  startSession: async function (camera, type) {
-    if (this.sipSession || this.sessionRunning === true) {
-      return;
-    }
+		// Start listening for doorbell presses on each camera
+		allCameras.forEach((camera) => {
+			camera.onDoorbellPressed.subscribe(async () => {
+				if (!this.sipSession) {
+					await this.startSession(camera, 'ring');
+				}
+			});
+			this.toLog(`Actively listening for doorbell presses`);
+			//Check config value if node app should stream motion
+			if (this.config.ringStreamMotion) {
+				camera.onMotionDetected.subscribe(async (newMotion) => {
+					//NewMotion is a true false value indicating whether the motion is new based on the dings made in the last 65 seconds
+					// This prevents the stream from being triggered on startup because it would not be an active motion event.
+					if (!this.sipSession && newMotion) {
+						await this.startSession(camera, 'motion');
+					}
+				});
+				this.toLog(`Actively listening for Motion events`);
+			}
+		});
+	},
 
-    this.sessionRunning = true;
-    if(type === "ring"){
-      this.toLog(`${camera.name} had its doorbell rung! Preparing video stream.`);
-    }else if(type === "motion"){
-      this.toLog(`${camera.name} has sensed motion Preparing video stream.`);
-    }else{
-      this.toLog(`${camera.name} been summoned by something other than a ring or motion. (spooky) Preparing video stream.`); 
-    }
-    
+	startSession: async function (camera, type) {
+		if (this.sipSession || this.sessionRunning === true) {
+			return;
+		}
 
-    await this.cleanUpVideoStreamDirectory();
-    this.watchForStreamStarted();
+		this.sessionRunning = true;
+		if (type === 'ring') {
+			this.toLog(
+				`${camera.name} had its doorbell rung! Preparing video stream.`
+			);
+			if (this.config.ringWakesUpScreen) {
+				this.sendSocketNotification('SCREEN_WAKEUP');
+			}
+			this.sendSocketNotification('DOORBELL_RINGING');
+		} else if (type === 'motion') {
+			this.toLog(
+				`${camera.name} has sensed motion Preparing video stream.`
+			);
+			if (this.config.ringWakesUpScreen) {
+				this.sendSocketNotification('SCREEN_WAKEUP');
+			}
+			this.sendSocketNotification('MOTION_DETECTED');
+		} else {
+			this.toLog(
+				`${camera.name} been summoned by something other than a ring or motion. (spooky) Preparing video stream.`
+			);
+		}
 
-    const streamTimeOut = this.config.ringMinutesToStreamVideo * 60 * 1000;
-    this.sipSession = await camera.streamVideo({
-      output: [
-        "-preset",
-        "veryfast",
-        "-g",
-        "25",
-        "-sc_threshold",
-        "0",
-        "-f",
-        "hls",
-        "-hls_time",
-        "2",
-        "-hls_list_size",
-        "6",
-        "-hls_flags",
-        "delete_segments",
-        pathApi.join(this.videoOutputDirectory, this.audioPlaylistFile)
-      ]
-    });
-    this.sipSession.onCallEnded.subscribe(() => {
-      this.toLog(`${camera.name} video stream has ended`);
-      this.sendSocketNotification("VIDEO_STREAM_ENDED", null);
-      this.stopWatchingFile();
-      this.sipSession = null;
-      this.sessionRunning = false;
-    });
-    setTimeout(() => {
-      this.toLog(`timeout hit`);
-      if (this.sipSession) {
-        this.sipSession.stop();
-      }
-    }, streamTimeOut);
-  },
+		await this.cleanUpVideoStreamDirectory();
+		this.watchForStreamStarted();
 
-  stopWatchingFile: function () {
-    if (this.watcher) {
-      this.watcher.close();
-      this.watcher = null;
-    }
-  },
+		const streamTimeOut = this.config.ringMinutesToStreamVideo * 60 * 1000;
+		this.sipSession = await camera.streamVideo({
+			output: [
+				'-preset',
+				'veryfast',
+				'-g',
+				'25',
+				'-sc_threshold',
+				'0',
+				'-f',
+				'hls',
+				'-hls_time',
+				'2',
+				'-hls_list_size',
+				'6',
+				'-hls_flags',
+				'delete_segments',
+				pathApi.join(this.videoOutputDirectory, this.audioPlaylistFile),
+			],
+		});
+		this.sipSession.onCallEnded.subscribe(() => {
+			this.toLog(`${camera.name} video stream has ended`);
+			this.sendSocketNotification('VIDEO_STREAM_ENDED', null);
+			this.stopWatchingFile();
+			this.sipSession = null;
+			this.sessionRunning = false;
+		});
+		setTimeout(() => {
+			this.toLog(`timeout hit`);
+			if (this.sipSession) {
+				this.sipSession.stop();
+			}
+		}, streamTimeOut);
+	},
 
-  toLog: function (message) {
-    console.log(`MMM-Ring at (${new Date().toLocaleString()}): ${message}`);
-  },
+	stopWatchingFile: function () {
+		if (this.watcher) {
+			this.watcher.close();
+			this.watcher = null;
+		}
+	},
 
-  watchForStreamStarted: function () {
-    this.stopWatchingFile();
+	toLog: function (message) {
+		console.log(`MMM-Ring at (${new Date().toLocaleString()}): ${message}`);
+	},
 
-    // only watch for file for 15 seconds
-    setTimeout(() => this.stopWatchingFile(), 15 * 1000);
+	watchForStreamStarted: function () {
+		this.stopWatchingFile();
 
-    this.watcher = fileWatcher.watch(this.videoOutputDirectory, {
-      ignored: /(^|[\/\\])\../,
-      persistent: true
-    });
+		// only watch for file for 15 seconds
+		setTimeout(() => this.stopWatchingFile(), 15 * 1000);
 
-    this.watcher.on("add", (filePath) => {
-      var fileName = filePath.split("\\").pop().split("/").pop();
+		this.watcher = fileWatcher.watch(this.videoOutputDirectory, {
+			ignored: /(^|[\/\\])\../,
+			persistent: true,
+		});
 
-      if (fileName === this.audioPlaylistFile) {
-        this.stopWatchingFile();
-        this.sendSocketNotification("VIDEO_STREAM_AVAILABLE", null);
-      }
-    });
-  }
+		this.watcher.on('add', (filePath) => {
+			var fileName = filePath.split('\\').pop().split('/').pop();
+
+			if (fileName === this.audioPlaylistFile) {
+				this.stopWatchingFile();
+				this.sendSocketNotification('VIDEO_STREAM_AVAILABLE', null);
+			}
+		});
+	},
 });
